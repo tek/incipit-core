0d8a4c1d0b95dba21d09c86c53626773a3d3209d
Torsten Schmits <git@tryp.io>
2022-02-25 02:05:29 +0100

    add polysemy-plugin to flake overrides for version compat

--- flake.nix
+++ flake.nix
@@ -8,10 +8,12 @@
 
     all = { hackage, override, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
+      polysemy-plugin = hackage "0.4.1.0" "117g92l1ppsqd3w0rqjrxfk0lx6yndd54rpymgxljilnv43zg29s";
     };
 
     ghc921 = { hackage, ... }: {
       polysemy = hackage "1.7.1.0" "0qwli1kx3hk68hqsgw65mk81bx0djw1wlk17v8ggym7mf3lailyc";
+      polysemy-plugin = hackage "0.4.3.0" "1r7j1ffsd6z2q2fgpg78brl2gb0dg8r5ywfiwdrsjd2fxkinjcg1";
       type-errors = hackage "0.2.0.0" "19km8k3g5gn69vvpq0qi0wzsrcjz5k6kb6qjrccq7za39z9slr2z";
     };
 
