2a01da445537306cf000faf7e894505edb1bf31d
Torsten Schmits <git@tryp.io>
2023-06-01 21:44:01 +0200

    add type family Undefined

--- packages/incipit-base/lib/Incipit/TypeError.hs
+++ packages/incipit-base/lib/Incipit/TypeError.hs
@@ -5,20 +5,28 @@ module Incipit.TypeError where
 
 import GHC.TypeLits (ErrorMessage (..), Symbol)
 
+-- | Concat two error message strings.
 type (<>) :: k1 -> k2 -> ErrorMessage
 type family (<>) l r :: ErrorMessage where
   l <> r = ToErrorMessage l ':<>: ToErrorMessage r
 
 infixl 5 <>
 
+-- | Concat two error message strings with a newline.
 type (%) :: k1 -> k2 -> ErrorMessage
 type family (%) t b :: ErrorMessage where
   t % b = ToErrorMessage t ':$$: ToErrorMessage b
 
 infixr 4 %
 
+-- | Convert a 'Symbol', 'ErrorMessage' or type to an 'ErrorMessage'.
 type ToErrorMessage :: k -> ErrorMessage
 type family ToErrorMessage t where
   ToErrorMessage (t :: Symbol) = 'Text t
   ToErrorMessage (t :: ErrorMessage) = t
   ToErrorMessage t = 'ShowType t
+
+-- | Placeholder similar to 'IncipitBase.undefined'.
+type Undefined :: k
+type family Undefined where
+  Undefined = TypeError ('Text "undefined")
