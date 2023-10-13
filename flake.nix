3172f9d1c5ae19ec2c3858f2ca1f0ab779de89be
Torsten Schmits <git@tryp.io>
2023-10-13 21:07:51 +0200

    remove unused override

--- flake.nix
+++ flake.nix
@@ -3,14 +3,13 @@
 
   inputs.hix.url = "git+https://git.tryp.io/tek/hix";
 
-  outputs = { hix, ... }: hix.lib.pro ({config, ...}: {
+  outputs = {hix, ...}: hix.lib.pro ({config, ...}: {
     ghcVersions = ["ghc92" "ghc94" "ghc96"];
     main = "incipit-core";
     hackage.versionFile = "ops/version.nix";
     gen-overrides.enable = true;
 
     envs.ghc96.overrides = {hackage, jailbreak, ...}: {
-      polysemy = hackage "1.9.1.3" "0y339fh1jvjdjmw6xkwizd3m9bqsgnhaj56xgkz1pcmv00pmj275";
       type-errors = jailbreak;
     };
 
--- ops/overrides.nix
+++ ops/overrides.nix
@@ -6,38 +6,6 @@ ghc92 = {
 ghc94 = {
 };
 ghc96 = {
-  polysemy = {
-  meta = {
-    sha256 = "0y339fh1jvjdjmw6xkwizd3m9bqsgnhaj56xgkz1pcmv00pmj275";
-    ver = "1.9.1.3";
-  };
-  drv = { mkDerivation, async, base, Cabal, cabal-doctest, containers
-, doctest, first-class-families, hspec, hspec-discover
-, inspection-testing, lib, mtl, stm, syb, template-haskell
-, th-abstraction, transformers, type-errors, unagi-chan
-}:
-mkDerivation {
-  pname = "polysemy";
-  version = "1.9.1.3";
-  src = /nix/store/184xbj3g4w50pz6q56cslj0sd7lwr60s-source;
-  setupHaskellDepends = [ base Cabal cabal-doctest ];
-  libraryHaskellDepends = [
-    async base containers first-class-families mtl stm syb
-    template-haskell th-abstraction transformers type-errors unagi-chan
-  ];
-  testHaskellDepends = [
-    async base containers doctest first-class-families hspec
-    hspec-discover inspection-testing mtl stm syb template-haskell
-    th-abstraction transformers type-errors unagi-chan
-  ];
-  testToolDepends = [ hspec-discover ];
-  homepage = "https://github.com/polysemy-research/polysemy#readme";
-  description = "Higher-order, low-boilerplate free monads";
-  license = lib.licenses.bsd3;
-}
-;
-}
-;
 };
 hls = {
 };
